9040d62113858a15ab241dcac187409cacaa3331
Raldes Krisnu <raldeskrisnu@gmail.com>
2018-03-23 21:20:59 +0700

    change application properties to yaml

--- src/main/java/com/test/service/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/test/service/serviceregistry/ServiceRegistryApplication.java
@@ -3,11 +3,10 @@ package com.test.service.serviceregistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
-@EnableEurekaServer
+
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
